5374de368d270553413f322a67d719dd7297f798
Santiago Hernandez <santi84@gmail.com>
2024-08-17 10:22:44 -0700

    more aliases

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -1,10 +1,28 @@
-alias top=htop
-alias zedit="vim ~/.zshrc"
+GIT
 alias ga="git add"
 alias gc="git commit"
 alias pull="git pull"
 alias push="git push"
+
 alias cls='clear'
+alias c='clear'
 alias reload='. ~/.zshrc'
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-alias aliases="vim /Users/santius/.oh-my-zsh/custom/aliases.zsh"
+alias top=htop
+alias zedit="vim ~/.zshrc"
+alias ..='cd ..'
+alias ls='ls -la'
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias diff='colordiff'
+alias vi=vim
+alias ports='netstat -tulanp'
+alias meminfo='free -m -l -t'
+alias wget='wget -c'
+alias df='df -H'
+alias du='du -ch'
+
+
+#EDITS
+alias aliases="vim $ZSH_CUSTOM/aliases.zsh"
