01f0acbfa16f2e81de70a848c6b7f98ed088b05c
santius <santi84@gmail.com>
2024-10-24 13:00:58 -0700

    Deleted some files and added new aliases

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -3,12 +3,11 @@ alias ga="git add"
 alias gc="git commit"
 alias pull="git pull"
 alias push="git push"
-
 alias cls='clear'
 alias c='clear'
 alias reload='. ~/.zshrc'
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-alias top=htop
+alias top=btop
 alias zedit="vim ~/.zshrc"
 alias ..='cd ..'
 alias ls='ls -la'
@@ -21,8 +20,6 @@ alias ports='netstat -tulanp'
 alias meminfo='free -m -l -t'
 alias wget='wget -c'
 alias df='df -H'
-alias du='du -ch'
-
-
-#EDITS
+alias du='dust'
+alias cat="bat"
 alias aliases="vim $ZSH_CUSTOM/aliases.zsh"
